ec550b4bffe4e1cb6a93f33ea8605df076720ce4
javila-2021047@kinal.edu.gt <wsalguero-2021689@kinal.edu.gt>
2023-01-20 09:28:10 -0600

    V1 commit page

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+
+function sumar(){
+    let numA = document.getElementById("valorA").value;
+    let numB= document.getElementById("valorB").value
+    let resultado =  parseInt(numA)+ parseInt(numB);
+    console.log("Saludos");
+    console.log(resultado);
+}
